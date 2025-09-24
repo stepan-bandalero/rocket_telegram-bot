8d7ff1bbb650bfd48f9c95f615f8d646a7318b38
Sergey <sayvix005@gmail.com>
2025-09-24 21:13:05 +0300

    fix info manage channels

--- bot/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# bot/models/__init__.py
-from .users import User
-from .user_gift import UserGift
-from .gift_catalog import GiftCatalog
-from .gift_stakings import GiftStaking
--- bot/models/channels.py
+++ bot/models/channels.py
@@ -1,13 +1,8 @@
 from sqlalchemy import Column, Integer, BigInteger, Text, Boolean, TIMESTAMP, func
-from sqlalchemy.dialects.postgresql import Any
-
 from bot.db import Base
 
 
 class Channel(Base):
-    def __init__(self, **kw: Any):
-        super().__init__(kw)
-
     __tablename__ = "channels"
 
     id = Column(Integer, primary_key=True)
