3a092a86beddef295fbedf94d54421db7ae7b4ff
Sergey <sayvix005@gmail.com>
2025-10-06 01:30:09 +0300

    add test promos

--- bot/app.py
+++ bot/app.py
@@ -10,7 +10,7 @@ from aiogram.client.default import DefaultBotProperties
 
 from .config import settings
 from .middlewares.db import DataBaseSessionMiddleware
-from .handlers import start, admin, admin_promos, admin_channels, admin_broadcast, admin_users, admin_gift, admin_balance, system_stats, business_debug, gift_payout, ton_requests
+from .handlers import start, admin, admin_promos, admin_channels, admin_broadcast, admin_users, admin_gift, admin_balance, system_stats, business_debug, gift_payout, ton_requests, gift_promos
 
 sys.path.append(str(Path(__file__).resolve().parent))
 
@@ -45,6 +45,7 @@ async def main():
     dp.include_router(business_debug.router)
     dp.include_router(gift_payout.router)
     dp.include_router(ton_requests.router)
+    dp.include_router(gift_promos.router)
 
 
 
